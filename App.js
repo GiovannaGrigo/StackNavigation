902ca1d5e9c89334d0b1f7c9ca0ddf1ddbd30ed6
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-11-27 22:24:17 -0300

    Criação do sistema de tabs

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App(){
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({color, size}) => (
+          <Icon name="home" color={color} size={size}/>
+        )}} />
+        <Tab.Screen name="Details" component={DetailsScreen} options={{tabBarIcon: ({color, size}) => (
+          <Icon name="grid" color={color} size={size}/>
+        )}} />
+        </Tab.Navigator>
     </NavigationContainer>
   )
 }

0edfbd9b25a7308815d7e8210583211a7c2c6e5c
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-11-27 21:35:36 -0300

    Correção do projeto.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
@@ -11,8 +11,8 @@ export default function App(){
   return(
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Details" component={DetailsScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )

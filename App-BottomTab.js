2e8429133b674e7eba3d11634c669b391501ea2c
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-11-28 22:24:01 -0300

    Criação da Drawer

--- App-BottomTab.js
+++ App-BottomTab.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App(){
   return(
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} options={{drawerIcon: ({color, size}) => (
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({color, size}) => (
           <Icon name="home" color={color} size={size}/>
         )}} />
-        <Drawer.Screen name="Details" component={DetailsScreen} options={{drawerIcon: ({color, size}) => ( 
+        <Tab.Screen name="Details" component={DetailsScreen} options={{tabBarIcon: ({color, size}) => ( 
           <Icon name="grid" color={color} size={size}/>
         )}} />
-        </Drawer.Navigator>
+        </Tab.Navigator>
     </NavigationContainer>
   )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { Avatar } from 'react-native-paper';
 
-export default function HomeScreen({navigation}){
-    return(
+
+export default function HomeScreen({ navigation }) {
+    return (
         <View style={{
             flex: 1,
             alignItems: 'center',
             justifyContent: 'center'
-            }}>
-                <Text>Home Screen</Text>
-                <Button 
-                    title="Ir para detalhes"
-                    onPress={() => navigation.navigate('Details')}
-                    />
+        }}>
+            <Text>Home Screen</Text>
+            <Avatar.Icon size={24} icon="folder" />
+            <Button
+                title="Ir para detalhes"
+                onPress={() => navigation.navigate('Details')}
+            />
+
         </View>
     )
 }
